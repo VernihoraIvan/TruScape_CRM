3292071d3001cab199ef6eda4369ad5424dad176
VernihoraIvan <vernihorai@gmail.com>
2024-01-11 21:30:20 +0100

    initial routing created, layout added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import ActiveLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <main>
-      <h1>Home page</h1>
+      <h1 className="text-xl">Home page</h1>
+      <ActiveLabel status={Status.Active}>Active label</ActiveLabel>
+      <ActiveLabel status={Status.Nonactive} disabled={true}>
+        Nonactive label
+      </ActiveLabel>
+      <ActiveLabel status={Status.Pending}>Pending label</ActiveLabel>
+      <ActiveLabel status={Status.Suspended}>Suspended label</ActiveLabel>
     </main>
   );
 }
