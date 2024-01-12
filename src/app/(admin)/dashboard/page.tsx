91eec39d20009ee83c7041d03ab738cdfd3e0723
VernihoraIvan <vernihorai@gmail.com>
2024-01-12 18:25:51 +0100

    created utilities for fetching data, connected mockAPI, Categories component added

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -2,13 +2,16 @@ import MagicButton from '@/app/components/magic-button';
 import Link from 'next/link';
 import React from 'react';
 
-export interface PageProps {}
+export interface PageProps {
+  children: React.ReactNode;
+}
 
-export default function Page({}: PageProps) {
+export default function Page({ children }: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
       {/* <MagicButton /> */}
+      <div>{children}</div>
     </main>
   );
 }
