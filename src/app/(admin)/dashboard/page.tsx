7fd9b77637cf676f17d9425c331d5495550c068c
VernihoraIvan <vernihorai@gmail.com>
2024-01-12 14:54:48 +0100

    error handling created, profile header added

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import MagicButton from '@/app/components/magic-button';
+import Link from 'next/link';
 import React from 'react';
 
 export interface PageProps {}
@@ -6,6 +8,7 @@ export default function Page({}: PageProps) {
   return (
     <main>
       <h1 className="text-xl">Dashboard Page</h1>
+      {/* <MagicButton /> */}
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import ActiveLabel, { Status } from './components/status-label';
+import Link from 'next/link';
+import MagicButton from './components/magic-button';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <ActiveLabel status={Status.Active}>Active label</ActiveLabel>
-      <ActiveLabel status={Status.Nonactive} disabled={true}>
-        Nonactive label
-      </ActiveLabel>
-      <ActiveLabel status={Status.Pending}>Pending label</ActiveLabel>
-      <ActiveLabel status={Status.Suspended}>Suspended label</ActiveLabel>
+      {/* <MagicButton /> */}
+      <Link href="/dashboard">To Dashboard</Link>
     </main>
   );
 }
