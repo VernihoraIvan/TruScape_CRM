e3cdd7f5919caf5ff2b929348524a8eed6970818
VernihoraIvan <vernihorai@gmail.com>
2024-02-03 11:14:51 +0100

    fixed bug with data fetching

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -11,14 +11,14 @@ export default function Layout({ categories, children }: LayoutProps) {
     <div>
       {children}
       {/*Header */}
-      <main className="grid grid-cols-12 gap-5 py-10 pl-10 pr-7">
+      <main className="  gap-5 py-10 pl-10 pr-7">
         <div>{categories}</div> {/*small segment element*/}
         <div>Layout inside Dashboard</div>
-        <div className="col-span-12">stats</div>
+        {/* <div className="col-span-12">stats</div>
         <div className="col-span-5">sales</div>
         <div className="col-span-7">categories</div>
         <div className="col-span-6">countries</div>
-        <div className="col-span-6">promotions</div>
+        <div className="col-span-6">promotions</div> */}
       </main>
     </div>
   );
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -68,7 +68,7 @@ const stringifyQueryParams = (params: Record<string, string>) =>
 
 const sendRequest = async <T>(url: string, init?: RequestInit) => {
   const res = await fetch(url, init);
-  console.log(res);
+  // console.log(res);
   if (!res.ok) {
     throw new Error(await res.text());
   }
