07f75d22409fdcdfe2a1331b065a48fbc66e1bcb
VernihoraIvan <vernihorai@gmail.com>
2024-02-03 14:05:50 +0100

    Add new company refactored to a modal window

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import Button from '@/app/components/button';
+import React from "react";
+import { useRouter } from "next/navigation";
+import Button from "@/app/components/button";
 
 export default function AddCompanyButton() {
   const router = useRouter();
   return (
-    <Button onClick={() => router.push('/companies/new', { scroll: false })}>
+    <Button onClick={() => router.push("/companies/new", { scroll: false })}>
       Add company
     </Button>
   );
