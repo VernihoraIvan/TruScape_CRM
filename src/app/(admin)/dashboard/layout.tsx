d25978cf92578629e96435b4872f825f37a6280e
VernihoraIvan <vernihorai@gmail.com>
2024-02-03 15:30:12 +0100

    Countries of companies component added

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -4,20 +4,26 @@ import React from "react";
 export interface LayoutProps {
   children: React.ReactNode;
   categories: React.ReactNode;
+  countries: React.ReactNode;
 }
 
-export default function Layout({ categories, children }: LayoutProps) {
+export default function Layout({
+  categories,
+  children,
+  countries,
+}: LayoutProps) {
   return (
     <div>
       {children}
       {/*Header */}
       <main className="  gap-5 py-10 pl-10 pr-7">
         <div>{categories}</div> {/*small segment element*/}
+        <div className="col-span-6">{countries}</div>
         <div>Layout inside Dashboard</div>
         {/* <div className="col-span-12">stats</div>
         <div className="col-span-5">sales</div>
         <div className="col-span-7">categories</div>
-        <div className="col-span-6">countries</div>
+       
         <div className="col-span-6">promotions</div> */}
       </main>
     </div>
