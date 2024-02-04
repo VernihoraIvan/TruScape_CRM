fba7c134f1cf8be6df4e7718d25d73087935c9fb
VernihoraIvan <vernihorai@gmail.com>
2024-02-04 14:15:42 +0100

    sales component added

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -6,6 +6,7 @@ export interface LayoutProps {
   categories: React.ReactNode;
   countries: React.ReactNode;
   promotions: React.ReactNode;
+  sales: React.ReactNode;
 }
 
 export default function Layout({
@@ -13,17 +14,20 @@ export default function Layout({
   children,
   countries,
   promotions,
+  sales,
 }: LayoutProps) {
   return (
     <div>
       {children}
       {/*Header */}
-      <main className="  gap-5 py-10 pl-10 pr-7">
-        <div>{categories}</div> {/*small segment element*/}
+      <main className="grid grid-cols-12 gap-5 py-10 pl-10 pr-7">
+        {/* <div className="col-span-12">{stats}</div> */}
+        <div className="col-span-5">{sales}</div>
+        <div className="col-span-7">{categories}</div>
         <div className="col-span-6">{countries}</div>
         <div className="col-span-6">{promotions}</div>
         {/* <div className="col-span-12">stats</div>
-        <div className="col-span-5">sales</div>
+        
         <div className="col-span-7">categories</div>
        
          */}
